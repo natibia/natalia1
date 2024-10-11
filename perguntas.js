7e76756b65c7147970220838118b6248ec47857f
natibia <142630925+natibia@users.noreply.github.com>
2024-10-11 09:36:02 -0300

    ss

--- perguntas.js
+++ perguntas.js
@@ -1,79 +1,79 @@
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Matemática',
+    'Qual a ordem de prioridade em expressão numérica?',
+    'Parênteses, colchetes e chaves'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Biologia',
+    'Quais são os componentes orgânicos da célula?',
+    'São 6. Glicídios, lipídios, proteínas, enzimas, vitaminas e ácidos nucléicos.'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Química',
+    'O que é destilação fracionada?',
+    'Separação pelo ponto específico de ebulição de cada substância.'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Português',
+    'Quais são os pronomes pessoais retos?',
+    'eu, nós, tu, vós, ele(s), ela(s)'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Biologia',
+    'O que é plasmídeo?',
+    'Material genético circular das células procarionte, é o que confere a resistência das bactérias contra os antibióticos.'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Português',
+    'Quais são os pronomes oblíquos átonos?',
+    'me, nos, te, vos, se, lhe(s), o(s), a(s)'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Inglês',
+    'Quais são as formas do verbo to be?',
+    'am, are, is'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Filosofia',
+    'Teoria de Kant acerca do entendimento humano',
+    'Razão pura'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Geografia',
+    'O que foi a Guerra Fria?',
+    'Foi uma série de conflitos indiretos entre E.U.A. e a URSS.'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'História',
+    'O que foi o Governo Provisório',
+    'Período da Era Vargas de 1930 a 1934 (pós-golpe)'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Física',
+    'O que é referencial?',
+    'é o que se toma por base para avaliar se um corpo está em repouso ou em movimento.'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Geografia',
+    'O Brasil é uma federação de 1º, 2º ou 3º grau?',
+    '3º grau'
 )
 
 criaCartao(
-    'Matmática',
-    'Pergunta',
-    'Resposta'
+    'Filosofia',
+    'O que é moralismo?',
+    'é uma prática do indivíduo que reage tentando impor ao outro seus valores.'
 )
 
 criaCartao(
