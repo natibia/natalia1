7629cf86bfcc2541efd076828646b99c4dba30ef
natibia <142630925+natibia@users.noreply.github.com>
2024-10-18 08:37:56 -0300

    ds

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function criaCartao (categoria, pergunta, resposta){
     let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    
+    `
 }
