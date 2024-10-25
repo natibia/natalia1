3ca4fc1983334ff4489e00cf32666ac7c33b990e
natibia <142630925+natibia@users.noreply.github.com>
2024-10-25 12:23:17 -0300

    a

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick="mostrarResposta(this)">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
@@ -18,4 +18,10 @@ function criaCartao (categoria, pergunta, resposta){
 
 function mostrarResposta(){
     const resposta = document.querySelector('.resposta-cartao');
+    if(resposta.style.display === 'none'){
+        resposta.style.display = 'block'
+    }
+    else{
+        resposta.style.display = 'none'
+    }
 }
