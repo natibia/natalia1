889d5cce12f4a4a384c4664cdb27e7146f1dde8c
natibia <142630925+natibia@users.noreply.github.com>
2024-11-01 11:33:57 -0300

    v

--- pagina2.js
+++ pagina2.js
@@ -16,7 +16,7 @@ function criaCartao (categoria, pergunta, resposta){
     container.appendChild(cartao);
 }
 
-function mostrarResposta(){
+function mostrarResposta(card){
     const resposta = document.querySelector('.resposta-cartao');
     if(resposta.style.display === 'none'){
         resposta.style.display = 'block'
